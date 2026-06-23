17b88651143397b35e14a7c663a032703c31ea3f
Jasonls221 <jasonls221@gmail.com>
2026-06-22 20:37:22 -0400

    Mostly fixed max brightness at high PWM

--- keyboards/redragon/k582/config.h
+++ keyboards/redragon/k582/config.h
@@ -35,7 +35,7 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 2
 
 /* Increase PWM, lower to increase max brightness */
-#define RGB_MATRIX_SPD_STEP 48
+#define RGB_MATRIX_SPD_STEP 96
 
 /* Stable branch RGB driver compatibility shims */
 #define LED_MATRIX_ROWS MATRIX_ROWS
